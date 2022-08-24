d8ad119d3041834c25bb008bf87d75e537044937
Malka <malkaanjum1999@gmail.com>
2022-08-24 17:10:23 +0530

    main code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState();
+
+  const updateName = (event) => {
+    setName(event.target.value);
+  };
+
+  const onSubmit = () => {
+    setFullName(name);
+  };
   return (
     <div className="App">
-      <h1>hello</h1>
-      <input type='text' placeholder='Enter You Name' />
-      <button type='submit'>Submit</button>
+      <div className="content">
+        <h1>
+          Hello {fullName}{" "}
+          <img
+            src="https://raw.githubusercontent.com/nixin72/nixin72/master/wave.gif"
+            alt="Waving hand animated gif"
+            height="45"
+            width="45"
+          />
+        </h1>
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter You Name"
+          onChange={updateName}
+        />
+        <button onClick={onSubmit} type="submit">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }

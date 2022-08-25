e3351eefcbe492848e6729157410a4b5563137dd
Malka <malkaanjum1999@gmail.com>
2022-08-25 11:43:15 +0530

    added lastname input

--- src/App.js
+++ src/App.js
@@ -2,37 +2,52 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  const [fullName, setFullName] = useState();
+  const [last, setlast] = useState("")
+  const [fullName, setFullName] = useState("");
+  const [lastName, setlastName] = useState("")
 
   const updateName = (event) => {
     setName(event.target.value);
   };
 
-  const onSubmit = () => {
+  const updatePassword = (event) =>{
+    setlast(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
     setFullName(name);
+    setlastName(last)
   };
   return (
     <div className="App">
-      <div className="content">
-        <h1>
-          Hello {fullName}{" "}
-          <img
-            src="https://raw.githubusercontent.com/nixin72/nixin72/master/wave.gif"
-            alt="Waving hand animated gif"
-            height="45"
-            width="45"
+      <form onSubmit={onSubmit}>
+          <h1>
+            Hello {fullName} {lastName}
+            <img
+              src="https://raw.githubusercontent.com/nixin72/nixin72/master/wave.gif"
+              alt="Waving hand animated gif"
+              height="45"
+              width="45"
+            />
+          </h1>
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter Your Name"
+            onChange={updateName}
           />
-        </h1>
-        <input
-          type="text"
-          value={name}
-          placeholder="Enter You Name"
-          onChange={updateName}
-        />
-        <button onClick={onSubmit} type="submit">
-          Submit
-        </button>
-      </div>
+          <input
+            type="text"
+            value={last}
+            placeholder="Enter Your Password"
+            onChange={updatePassword}
+          />
+          <button type="submit">
+            Submit
+          </button>
+      </form>
+
     </div>
   );
 }
